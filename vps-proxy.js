6d608e4201963ab4ade2b9f1ca52bd9b7feed399
Krishna Avtar <krishnaavtar955@gmail.com>
2025-12-03 10:18:37 +0530

    change port

--- vps-proxy.js
+++ vps-proxy.js
@@ -9,7 +9,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PROXY_PORT || 3000;
+const PORT = process.env.PROXY_PORT || 3001;
 
 // Middleware
 app.use(express.json({ limit: "10mb" }));
